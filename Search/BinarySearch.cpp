c68c3dc657ac46744604df2d244f9522d01ebcc0
amir ardroudi <a.ardroudi@gmail.com>
2017-11-07 03:07:16 +0330

    binary search added

---
+++ Search/BinarySearch.cpp
@@ -0,0 +1,42 @@
+// ---------------------------------------------------
+//						            Algorithms
+//					**** Binary Search ****
+//
+//			professor : samad najar
+//			date   : nov 6,2017                           
+//			author : Amir Ardroudi
+//
+// ---------------------------------------------------
+
+#include <iostream>
+using namespace std;
+
+// ---------------- Recursive implementation ----------------
+int BinarySearch(int arr[],int value, int low, int high)
+{
+
+  //not found your number
+  if(low > high)
+    return -1;
+  int mid = low + (high - 1)/2;
+
+  if(arr[mid] > value)
+    return BinarySearch(arr, value, low, mid-1);
+
+  else if(arr[mid] < value)
+    return BinarySearch(arr, value, mid+1, high)
+  else
+    return mid; //found
+}
+// ---------------- Main ---------------
+int main()
+{
+  int arr[] =  {1, 2, 4, 5, 8, 9, 11, 22, 23, 28, 32};
+  int n = sizeof(arr)/sizeof(arr[]);
+  cout<<"Search your number from this arr : 1, 2, 4, 5, 8, 9, 11, 22, 23, 28, 32"<<"\n";
+  int x;
+  cin>>x;
+  cout<<"your number location : "<<BinarySearch(arr,x ,0 ,n-1);
+
+  return 0;
+}
