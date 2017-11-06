360d51cd090acca355b3a31e34e0f3a061c6fbc4
amir ardroudi <a.ardroudi@gmail.com>
2017-11-07 03:18:59 +0330

    minor changes

--- Search/BinarySearch.cpp
+++ Search/BinarySearch.cpp
@@ -3,7 +3,7 @@
 //					**** Binary Search ****
 //
 //			professor : samad najar
-//			date   : nov 6,2017                           
+//			date   : nov 6,2017
 //			author : Amir Ardroudi
 //
 // ---------------------------------------------------
@@ -12,31 +12,32 @@
 using namespace std;
 
 // ---------------- Recursive implementation ----------------
-int BinarySearch(int arr[],int value, int low, int high)
+int BinarySearch(int arr[], int value, int low, int high)
 {
 
-  //not found your number
-  if(low > high)
-    return -1;
-  int mid = low + (high - 1)/2;
+	//not found your number
+	if (low < high)
+	{
+		int mid = low + (high - 1) / 2;
 
-  if(arr[mid] > value)
-    return BinarySearch(arr, value, low, mid-1);
+		if (arr[mid] > value)
+			return BinarySearch(arr, value, low, mid - 1);
 
-  else if(arr[mid] < value)
-    return BinarySearch(arr, value, mid+1, high)
-  else
-    return mid; //found
+		else if (arr[mid] < value)
+			return BinarySearch(arr, value, mid + 1, high);
+		else
+			return mid; //found
+	}
 }
 // ---------------- Main ---------------
 int main()
 {
-  int arr[] =  {1, 2, 4, 5, 8, 9, 11, 22, 23, 28, 32};
-  int n = sizeof(arr)/sizeof(arr[]);
-  cout<<"Search your number from this arr : 1, 2, 4, 5, 8, 9, 11, 22, 23, 28, 32"<<"\n";
-  int x;
-  cin>>x;
-  cout<<"your number location : "<<BinarySearch(arr,x ,0 ,n-1);
+	int arr[] = { 1, 2, 4, 5, 8, 9, 11, 22, 23, 28, 32 };
+	int n = sizeof(arr)/sizeof(arr[0]);
+	cout << "Search your number from this arr : 1, 2, 4, 5, 8, 9, 11, 22, 23, 28, 32" << "\n";
+	int x;
+	cin >> x;
+	cout << "your number location : " << BinarySearch(arr, x, 0, n - 1) << endl;
 
-  return 0;
+	return 0;
 }
