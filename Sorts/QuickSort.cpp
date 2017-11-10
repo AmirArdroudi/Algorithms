9a8853fb55d6e84eaa2e7d42dbc270f665ae2733
amir ardroudi <a.ardroudi@gmail.com>
2017-11-10 21:24:37 +0330

    QuickSort Added

---
+++ Sorts/QuickSort.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int arr[1000];
+
+void swap(int *a, int *b)
+{
+  int temp = *a;
+  *a = *b;
+  *b = temp;
+}
+
+void quickSort(int arr[], int left, int right)
+{
+  int i,j,pivot;
+  if (left < right)
+  {
+    i = left;
+    j = right + 1;
+    pivot = arr[left];
+    cout<<" pivot : "<<arr[left]<<"   ";
+    do{
+        do{
+            i++;
+        }while(arr[i] < pivot);
+
+        do{
+            j--;
+        }while(arr[j] > pivot);
+
+        if(i < j)
+            swap(&arr[i], &arr[j]);
+
+    }while(i < j);
+    swap(&arr[left], &arr[j]);
+    quickSort(arr, j + 1, right);
+    quickSort(arr, left, j-1);
+
+  }
+
+
+}
+
+
+int main()
+{
+  int arr[] = {26, 5, 37, 1, 61, 11, 59, 15, 48, 19};
+  int n = sizeof(arr)/sizeof(arr[0]);
+  quickSort(arr, 0 , n -1);
+  for(int i= 0; i < n; i++)
+  {
+      cout<<arr[i]<<"  ";
+
+  }
+
+
+  return 0;
+}
